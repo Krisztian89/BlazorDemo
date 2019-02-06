50cb2eea82b2cf41620e3cec767a6db4cac9fac5
Krisztian Kovacs <hekky26@gmail.com>
2019-02-06 11:08:41 +0100

    Interface for order repository

---
+++ BlazorDemo.Server/DataAccess/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlazorDemo.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Server.DataAccess
+{
+    public interface IOrderRepository
+    {
+        IQueryable<Order> Orders { get; }
+        IQueryable<OrderItem> OrderItems { get; }
+
+        void AddNewOrder(Order order);
+        void AddNewOrderItem(OrderItem orderItem);
+    }
+}
