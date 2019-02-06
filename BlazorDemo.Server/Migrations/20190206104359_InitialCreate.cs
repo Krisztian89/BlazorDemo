0431fc3815a1ec84e167b1dc0f3f740a3f9daedd
Krisztian Kovacs <hekky26@gmail.com>
2019-02-06 13:20:05 +0100

    Migration - initial

---
+++ BlazorDemo.Server/Migrations/20190206104359_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorDemo.Server.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ORDER",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Comment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORDER", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ORDERITEM",
+                columns: table => new
+                {
+                    OrderItemID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ProductName = table.Column<string>(nullable: true),
+                    Quantity = table.Column<int>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    OrderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORDERITEM", x => x.OrderItemID);
+                    table.ForeignKey(
+                        name: "FK_ORDERITEM_ORDER_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "ORDER",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ORDERITEM_OrderId",
+                table: "ORDERITEM",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ORDERITEM");
+
+            migrationBuilder.DropTable(
+                name: "ORDER");
+        }
+    }
+}
