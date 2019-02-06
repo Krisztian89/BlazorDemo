5936f9128f111d127c3285b266124361bf2e74bc
Krisztian Kovacs <hekky26@gmail.com>
2019-02-06 11:04:04 +0100

    OrderContext created and implemented

---
+++ BlazorDemo.Server/DataAccess/OrderContext.cs
@@ -0,0 +1,25 @@
+﻿using BlazorDemo.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo.Server.DataAccess
+{
+    public class OrderContext : DbContext
+    {
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>().ToTable("ORDER");
+            modelBuilder.Entity<OrderItem>().ToTable("ORDERITEM");
+        }
+    }
+}
