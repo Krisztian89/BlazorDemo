cfe7ace565863f2695d3dcdc727ba2cee695e4f2
Krisztian Kovacs <hekky26@gmail.com>
2019-02-06 13:45:38 +0100

    Empty OrderController with repo created

---
+++ BlazorDemo.Server/Controllers/OrderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDemo.Server.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDemo.Server.Controllers
+{
+    public class OrderController : Controller
+    {
+        IOrderRepository repository;
+
+        public OrderController(IOrderRepository repo)
+        {
+            repository = repo;
+        }
+    }
+}
